e3432c79a9be8ea9245b220432b4799f0fc5384d
Mehki Joseph <disismehki@gmail.com>
2024-07-14 21:41:18 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def spark(crystals = 0, tickets = 0):
+    spark = 90000
+    ticket_value = 300
+    total = crystals + (tickets * ticket_value)
+    remaining_tickets = round((spark - total) / ticket_value)
+
+    print(f"Total value of your crystals and tickets: {total}")
+    print(f"Amount of crystals remaining until spark: {spark - total}")
+    print(f"The amount of tickets you would need to spark: {remaining_tickets}")
+
+    if total >= spark:
+        print("Your progress: 100%")
+    else:
+        print(f"Your progress: {float(total / spark * 100):.2f}%")
+
+def main():
+    crystals = int(input("How many crystals do you have?\n"))
+    tickets = int(input("How many tickets do you have?\n"))
+
+    spark(crystals, tickets)
+
+main()
